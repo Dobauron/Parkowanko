5df72830d45458598460f15d6b0a958cc9f84200
Dobauron <safetysafetystrazak@gmail.com>
2025-01-24 16:44:30 +0100

    updating name of app

--- ParkowankoAPI/urls.py
+++ ParkowankoAPI/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ParkownakoAPI project.
+URL configuration for ParkowankoAPI project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
