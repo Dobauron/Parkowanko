56b479f2a984a9db8dda0fc289c1b6b7a731fe3e
Dobauron <safetysafetystrazak@gmail.com>
2025-12-14 20:50:47 +0100

    registration data in return update

--- auth_system/services/auth.py
+++ auth_system/services/auth.py
@@ -5,12 +5,8 @@ def build_auth_response(user):
     refresh = RefreshToken.for_user(user)
 
     return {
-        "user": {
-            "id": user.id,
-            "username": user.username,
-        },
-        "tokens": {
-            "refresh": str(refresh),
-            "access": str(refresh.access_token),
-        },
+        "id": user.id,
+        "username": user.username,
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
     }
