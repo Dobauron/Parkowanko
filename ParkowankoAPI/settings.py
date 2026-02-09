713961a17edf6a82ac97632a81f613b9a0c26b0b
Dobauron <safetysafetystrazak@gmail.com>
2026-02-09 21:46:41 +0100

    login via facebook and update tests, add adapter for username field = email

--- ParkowankoAPI/settings.py
+++ ParkowankoAPI/settings.py
@@ -205,7 +205,7 @@ if DEBUG:
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-    EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+    EMAIL_PORT = config("EMAIL_PORT", default=465, cast=int)
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = config("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
