e8b7c5ee02e975a4494fe970fcc160f2fce29c94
Dobauron <safetysafetystrazak@gmail.com>
2026-02-16 16:16:33 +0100

    verification user email, sett language = PL, set production frontend url,

--- auth_system/api/serializers.py
+++ auth_system/api/serializers.py
@@ -7,12 +7,20 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth.models import Group
 from auth_system.services.auth import build_user_payload
 from rest_framework_simplejwt.serializers import TokenRefreshSerializer
+from allauth.account.models import EmailAddress
+from rest_framework.exceptions import AuthenticationFailed
 
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
     def validate(self, attrs):
         data = super().validate(attrs)
+
+        # Sprawdź weryfikację e-maila, jeśli jest wymagana
+        if getattr(settings, 'ACCOUNT_EMAIL_VERIFICATION', 'none') == 'mandatory':
+            email_address = EmailAddress.objects.filter(user=self.user, email=self.user.email).first()
+            if not email_address or not email_address.verified:
+                raise AuthenticationFailed("Adres e-mail nie został zweryfikowany.")
 
         data["expires_in"] = int(
             settings.SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"].total_seconds()
